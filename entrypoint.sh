0eaf24d487aa5eb2b45bba405c12f406d0489cee
joshuachoy <joshuachoy@users.noreply.github.com>
2019-10-24 15:36:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,13 @@ export OVERLAY_S3URL="s3://${BUCKET_NAME}/${LAMBDA_FUNC_NAME}/lambda-deploy.tgz"
 rm -f lambda-deploy.zip
 tar -czvf lambda-deploy-overlay.tgz ./
 aws s3 cp --acl public-read lambda-deploy-overlay.tgz "$OVERLAY_S3URL"
-cd lambda_functions; zip -r ../lambda-deploy.zip *
-cd ..
-cd ..
+cd industries/lambda_functions; zip -r ../lambda-deploy.zip *
+cd ../..
 
+echo "Debug"
 pwd
 ls -la .
+echo "******"
 aws cloudformation validate-template \
     --template-body file://template.yaml
  

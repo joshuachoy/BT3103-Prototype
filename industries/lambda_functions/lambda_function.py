e35f08bdf28e9a97d273fbe8a234d154006164d0
Vanessa Leong <51442464+vannnl@users.noreply.github.com>
2019-11-05 17:31:38 +0800

    Update lambda_function.py

--- industries/lambda_functions/lambda_function.py
+++ industries/lambda_functions/lambda_function.py
@@ -232,15 +232,15 @@ def lambda_handler(event, context):
 
         allTestCases = {"question 1": ">>> df.shape\n(500,8)",                                              #! Edited Here NOTE THAT FOR TUPLES, CANNOT LEAVE SPACE AFTER COMMA!
                         "question 2": ">>> df.head()\n" + df.head().to_string().replace("\n", "--"),
-                        "question 3": ">>> df.mean()\n" + pd.DataFrame(df.mean(), columns = ["Mean"]).to_string().replace("\n", "--")
-                        #"question 3": ">>> df.mean()\n" + df.mean().to_string().replace("\n", "--")
+                        #"question 3": ">>> df.mean()\n" + pd.DataFrame(df.mean(), columns = ["Mean"]).to_string().replace("\n", "--")
+                        "question 3": ">>> df['amount'].mean()\n" + df['amount'].mean()
                         }
         
         
         
         questionType = {"question 1": "non-dataframe",
                         "question 2": "dataframe",
-                        "question 3": "series"}
+                        "question 3": "non-dataframe"}
                         
         testCases = allTestCases[question] 
         #testCases = ">>> df.shape\n(500,8)"                                     #! Edited Here
